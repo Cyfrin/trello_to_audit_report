8f33652b823c94863c286925aaf9555849358fd7
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2023-03-22 09:39:09 -0400

    removed requests, upgraded

--- trello_to_audit_report/__version__.py
+++ trello_to_audit_report/__version__.py
@@ -2,7 +2,7 @@ __title__ = "trello_to_audit_report"
 __description__ = (
     "Python CLI for generating a nice looking audit from a project management tool."
 )
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Cyfrin"
 __license__ = "MIT"
 __copyright__ = "Copyright 2023 Cyfrin"
